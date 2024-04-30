3d666074721218518833ceee775698f317721f79
OsheeSan <osheee.san@gmail.com>
2024-04-30 14:35:15 +0300

    Initial Commit

---
+++ TestOBRIO/Controllers/BallanceVC.swift
@@ -0,0 +1,20 @@
+//
+//  BallanceVC.swift
+//  TestOBRIO
+//
+//  Created by admin on 30.04.2024.
+//
+
+import UIKit
+
+class BallanceVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
---
+++ TestOBRIO/Controllers/TransactionVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestOBRIO
+//
+//  Created by admin on 30.04.2024.
+//
+
+import UIKit
+
+class TransactionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
